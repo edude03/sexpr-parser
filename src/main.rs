8caef3ad860bf703c1911cd31a3a92473a601d1e
Michael Francis <edude03@gmail.com>
2021-01-24 15:17:44 -0500

    partial error handling, added notes

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,11 @@ fn main() {
     println!("{:?}", expr);
 }
 
-fn to_expr(v: Value) -> Box<Expr> {
+// One potential optimization is to pass a &mut Value so that
+// I could use .take() instead of clone. For a problem of this size
+// It doesn't matter, but if the tree was large and more complex that would
+// be a pretty easy optimization
+fn to_expr(v: Value) -> Result<Box<Expr>, &'static String> {
     use serde_json::Value::*;
 
     let res = match v {
@@ -49,8 +53,14 @@ fn to_expr(v: Value) -> Box<Expr> {
             // it's because I can't use the rest pattern if `v` is the object instead of pointers
             // because the size of the object isn't know at compile time
             [String(s), x, rest @ ..] if rest.len() > 1 => {
-                let exprs: Vec<Box<Expr>> = rest.iter().cloned().map(to_expr).collect();
-                Expr::VariadicEqual(to_expr(x.to_owned()), exprs)
+                // Look what rustfmt did to my boy :'(
+                let exprs: Vec<Box<Expr>> = rest.iter().cloned().map(to_expr).collect::<Result<
+                    Vec<Box<Expr>>,
+                    &'static std::string::String,
+                >>(
+                )?;
+
+                Expr::VariadicEqual(to_expr(x.to_owned())?, exprs)
             }
             [String(s), x, y] => {
                 let expr = match s.as_str() {
@@ -60,11 +70,12 @@ fn to_expr(v: Value) -> Box<Expr> {
                     ">" => Expr::GreaterThan,
                     "<" => Expr::LessThan,
                     "=" => Expr::EqualTo,
-                    _ => panic!(),
+                    _ => panic!(), // Unimplemented operand
                 };
-                expr(to_expr(x.to_owned()), to_expr(y.to_owned()))
+                expr(to_expr(x.to_owned())?, to_expr(y.to_owned())?)
             }
             // Bad way of checking if it's a Field type
+            // TODO, handle the error
             [String(s), Number(i)] => Expr::Field(i.as_i64().unwrap() as i32),
             // Bad way of checking if it's a macro
             [String(s), String(n)] => Expr::Macro(n.to_owned()),
@@ -74,7 +85,7 @@ fn to_expr(v: Value) -> Box<Expr> {
         },
     };
 
-    Box::new(res)
+    Ok(Box::new(res))
 }
 
 #[cfg(test)]
@@ -87,7 +98,7 @@ mod tests {
         let v = serde_json::from_str(r#"[">", ["field", 4], 35]"#).unwrap();
 
         assert_eq!(
-            to_expr(v),
+            to_expr(v).unwrap(),
             Box::new(GreaterThan(Box::new(Field(4)), Box::new(Int(35))))
         )
     }
@@ -99,7 +110,7 @@ mod tests {
                 .unwrap();
 
         assert_eq!(
-            to_expr(v),
+            to_expr(v).unwrap(),
             Box::new(And(
                 Box::new(LessThan(Box::new(Field(1)), Box::new(Int(5)))),
                 Box::new(EqualTo(Box::new(Field(2)), Box::new(String("joe".into())))),
@@ -115,7 +126,7 @@ mod tests {
         .unwrap();
 
         assert_eq!(
-            to_expr(v),
+            to_expr(v).unwrap(),
             Box::new(Or(
                 Box::new(NotEqualTo(
                     Box::new(Field(3)),
@@ -151,7 +162,7 @@ mod tests {
             serde_json::from_str(r#"["=", ["field", 3], 25, 26, 27]"#).unwrap();
 
         assert_eq!(
-            to_expr(v),
+            to_expr(v).unwrap(),
             Box::new(VariadicEqual(
                 Box::new(Field(3)),
                 vec![Box::new(Int(25)), Box::new(Int(26)), Box::new(Int(27))],
@@ -165,7 +176,7 @@ mod tests {
             .unwrap();
 
         assert_eq!(
-            to_expr(v),
+            to_expr(v).unwrap(),
             Box::new(And(
                 Box::new(LessThan(Box::new(Field(1)), Box::new(Int(5)))),
                 Box::new(Macro("is_joe".into())),
